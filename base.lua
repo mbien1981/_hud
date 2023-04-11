e6ee1102e173284225a480196eac6a1f3a1a446c
Uziel <clauselpitoloko@gmail.com>
2023-04-11 14:08:10 +0200

    Remove redundant /hud/ folder in hooks directory

--- base.lua
+++ base.lua
@@ -1,7 +1,7 @@
 local module = DMod:new("_hud", {
 	name = "_hud",
 	author = "_atom",
-	version = "1.3.4",
+	version = "1.3.5",
 	allow_globals = true,
 	dependency = "_sdk",
 	includes = {
@@ -11,19 +11,19 @@ local module = DMod:new("_hud", {
 	update = { id = "_hud", url = "https://raw.githubusercontent.com/mbien1981/dahm-modules/main/version.json" },
 })
 
-module:hook_post_require("lib/states/ingamewaitingforplayers", "hooks/hud/drop_in")
-module:hook_post_require("lib/managers/menumanager", "hooks/hud/drop_in")
+module:hook_post_require("lib/states/ingamewaitingforplayers", "hooks/drop_in")
+module:hook_post_require("lib/managers/menumanager", "hooks/drop_in")
 
-module:hook_post_require("lib/managers/hudmanager", "hooks/hud/ammo_panel")
+module:hook_post_require("lib/managers/hudmanager", "hooks/ammo_panel")
 
-module:hook_post_require("lib/states/ingamewaitingforplayers", "hooks/hud/state_timer")
+module:hook_post_require("lib/states/ingamewaitingforplayers", "hooks/state_timer")
 
-module:hook_post_require("lib/states/ingamewaitingforplayers", "hooks/hud/health_bar")
-module:hook_post_require("lib/units/beings/player/playerdamage", "hooks/hud/health_bar")
+module:hook_post_require("lib/states/ingamewaitingforplayers", "hooks/health_bar")
+module:hook_post_require("lib/units/beings/player/playerdamage", "hooks/health_bar")
 
-module:hook_post_require("lib/states/ingamewaitingforplayers", "hooks/hud/deployable_spy")
-module:hook_post_require("lib/units/equipment/ammo_bag/ammobagbase", "hooks/hud/deployable_spy")
-module:hook_post_require("lib/units/equipment/doctor_bag/doctorbagbase", "hooks/hud/deployable_spy")
-module:hook_post_require("lib/units/equipment/sentry_gun/sentrygunbase", "hooks/hud/deployable_spy")
+module:hook_post_require("lib/states/ingamewaitingforplayers", "hooks/deployable_spy")
+module:hook_post_require("lib/units/equipment/ammo_bag/ammobagbase", "hooks/deployable_spy")
+module:hook_post_require("lib/units/equipment/doctor_bag/doctorbagbase", "hooks/deployable_spy")
+module:hook_post_require("lib/units/equipment/sentry_gun/sentrygunbase", "hooks/deployable_spy")
 
 return module
