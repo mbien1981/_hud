bfbcb2a844917109c66d4bcbdb39618175fe701b
mbien1981 <shieldhater@gmail.com>
2024-04-05 04:58:19 +0200

    make deployable spy function in spectator mode

--- hooks/deployable_spy.lua
+++ hooks/deployable_spy.lua
@@ -9,7 +9,6 @@ function DeployableSpy:setup()
 	self._panel = self._ws:panel():panel({
 		name = "deployable_spy_panel",
 		layer = -100,
-		alpha = 1,
 		visible = true,
 	})
 
@@ -54,8 +53,8 @@ function DeployableSpy:add(unit, type)
 			text = "",
 			font = self.font.path,
 			font_size = self.font.size,
+			visible = false,
 			align = "center",
-			alpha = 0,
 			layer = 5,
 		}),
 	})
@@ -69,16 +68,11 @@ function DeployableSpy:remove(index)
 end
 
 function DeployableSpy:get_vector_angle(vector)
-	local p_unit = managers.player:player_unit()
-	if not alive(p_unit) then
-		return nil
-	end
-
 	local player = Vector3()
 	local target = Vector3()
 	local dir = Vector3()
 
-	mvector3.set(player, p_unit:camera():position())
+	mvector3.set(player, managers.viewport:get_current_camera_position())
 	mvector3.set(target, vector)
 	mvector3.set(dir, player)
 	mvector3.subtract(dir, target)
@@ -140,7 +134,8 @@ function DeployableSpy:get_item_text(unit, unit_type)
 	})
 end
 
-function DeployableSpy:update_item(index)
+local world_to_screen = Vector3()
+function DeployableSpy:update_item(index, camera)
 	local item = self.items[index]
 	if not alive(item.unit) or alive(item.unit) and item.unit:base()._empty then
 		self:remove(index)
@@ -148,15 +143,19 @@ function DeployableSpy:update_item(index)
 	end
 
 	local position = item.unit:position()
+	local camera_position = managers.viewport:get_current_camera_position()
+	local camera_rotation = managers.viewport:get_current_camera_rotation()
 
-	local camera = managers.player:player_unit():camera()
 	local angle = self:get_vector_angle(position)
-	if (mvector3.distance(camera:forward(), angle) / 2 * 360) > 180 then
-		item.text:set_alpha(0)
+	if (mvector3.distance(camera_rotation:y(), angle) / 2 * 360) > 180 then
+		if item.text:visible() then
+			item.text:hide()
+		end
 		return
 	end
 
 	local text = self:get_item_text(item.unit, item.type)
+
 	if not text then
 		self:remove(index)
 		return
@@ -165,36 +164,37 @@ function DeployableSpy:update_item(index)
 	item.text:set_text(text)
 	self._toolbox:parse_color_tags(item.text)
 
-	local distance = math.max(math.min(mvector3.distance(camera:position(), position), 8000), 20)
+	local distance = math.max(math.min(mvector3.distance(camera_position, position), 8000), 20)
 	item.text:set_font_size(36 / (distance / 200))
 
-	local world_to_screen = Vector3()
-	mvector3.set(world_to_screen, self._ws:world_to_screen(camera._camera_object, position))
+	mvector3.set(world_to_screen, self._ws:world_to_screen(camera, position))
+	item.text:set_center(mvector3.x(world_to_screen), mvector3.y(world_to_screen))
 
-	item.text:set_x(world_to_screen.x - (item.text:w() / 2))
-	item.text:set_y(world_to_screen.y)
-	item.text:set_alpha(1)
+	if not item.text:visible() then
+		item.text:show()
+	end
 end
 
-function DeployableSpy:update_items()
+function DeployableSpy:update_items(camera)
 	if not next(self.items) then
 		return
 	end
 
 	for index, _ in pairs(self.items) do
-		self:update_item(index)
+		self:update_item(index, camera)
 	end
 end
 
 function DeployableSpy:update()
-	if not alive(managers.player:player_unit()) or not D:conf("_hud_enable_deployable_spy") then
+	local camera = managers.viewport:get_current_camera()
+	if not alive(camera) or not D:conf("_hud_enable_deployable_spy") then
 		self._panel:hide()
 		return
 	end
 
 	self._panel:show()
 
-	self:update_items()
+	self:update_items(camera)
 end
 
 local module = ... or D:module("_hud")
@@ -204,11 +204,27 @@ if RequiredScript == "lib/units/equipment/ammo_bag/ammobagbase" then
 	module:post_hook(50, AmmoBagBase, "set_server_information", function(self, ...)
 		DeployableSpy:add(self._unit, "ammo_bag")
 	end)
+
+	module:post_hook(50, AmmoBagBase, "_take", function(self, ...)
+		DeployableSpy:add(self._unit, "ammo_bag")
+	end)
+
+	module:post_hook(50, AmmoBagBase, "sync_ammo_taken", function(self, ...)
+		DeployableSpy:add(self._unit, "ammo_bag")
+	end)
 end
 
 if RequiredScript == "lib/units/equipment/doctor_bag/doctorbagbase" then
 	local DoctorBagBase = module:hook_class("DoctorBagBase")
 	module:post_hook(50, DoctorBagBase, "set_server_information", function(self, ...)
+		DeployableSpy:add(self._unit, "medic_bag")
+	end)
+
+	module:post_hook(50, DoctorBagBase, "_take_ammo", function(self, ...)
+		DeployableSpy:add(self._unit, "medic_bag")
+	end)
+
+	module:post_hook(50, DoctorBagBase, "sync_taken", function(self, ...)
 		DeployableSpy:add(self._unit, "medic_bag")
 	end)
 end
