4f3856268148b7b041002dff2987b659393088e0
mbien1981 <shieldhater@gmail.com>
2024-04-09 04:03:00 +0200

    set version number to 1.5.4

--- base.lua
+++ base.lua
@@ -1,7 +1,7 @@
 local module = DMod:new("_hud", {
 	name = "_hud",
 	author = "_atom",
-	version = "1.5.3",
+	version = "1.5.4",
 	dependencies = { "hud", "[drop_in_menu]", "[loadout_dropdowns]", "[holiday_special]" },
 	includes = {
 		{ "dev/base" },
