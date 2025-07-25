b42a39bb866bfd49641de5ab708ee9e283b6165f
mbien1981 <shieldhater@gmail.com>
2025-07-26 00:10:52 +0200

    set version number to 1.7.11

--- base.lua
+++ base.lua
@@ -1,7 +1,7 @@
 return DMod:new("_hud", {
 	name = "_hud",
 	author = "_atom",
-	version = "1.7.10",
+	version = "1.7.11",
 	dependencies = { "hud", "[drop_in_menu]", "[loadout_dropdowns]", "[holiday_special]" },
 	includes = {
 		{ "dev/base" },
